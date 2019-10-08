c4322b3e28096708796aeb20b65c68c2feffc922
jiindev <jiindev@gmail.com>
2019-10-08 20:32:04 +0900

    JSX 특징 (문법 차이)
    - 자바스크립트 표현식을 작성하려면 JSX 내부에서 코드를 {}로 감싼다.
    - JSX 내부에서 if문을 사용할 순 없다. 조건에 따라 렌더링 해야할 때엔 조건부(삼항)연산자를 사용한다 condition ? '참':'거짓'
    - &&를 사용한 조건부 렌더링? condtion && '보여주세요'
    - 리액트에서 dom 요소에 스타일을 적용할 땐 css를 자바스크립트 객체 형식으로 만들어 적용한다. (key는 camelCase를 따름)
    - class 설정 시엔 class 대신 className을 사용해야 한다. (클래스가 자.스에 이미 존재하는 키워드이기 때문임)
    - JSX에선 태그를 늘 닫아주어야 함
    - { /*주석은 이렇게 사용한다*/}  || //self-closed태그에서 주석 작성시엔 이렇게 사용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, {Component, Fragment} from 'react';
+import './App.css';
 
 class App extends Component{
   render(){
+    const text = '당신은 어썸한가요?';
+    const condition = true;
+    const style = {
+      backgroundColor : 'grey',
+      border: '1px solid grey',
+      height : Math.round(Math.random() * 300)+50,
+      width : Math.round(Math.random() * 300)+50,
+      WebkitTransition : 'all',
+      MozTransition : 'all',
+      msTransition : 'all'
+    }
+    
     return(
-      <Fragment>
+      <div className="my-div">
+        {/*요소 밖에선 이렇게 작성합니다*/}
         <h1>리액트 안녕!</h1>
-        <h2>당신은 어썸한가요?</h2>
-      </Fragment>
+        <h2>{text}</h2>
+        {condition ? '참' : '거짓'}
+        {condition && '보여주세요'}
+        <div style={style}
+          // self-closed 태그에서만 작동하는 주석
+          // 마지막 />가 꼭 새 줄에 있어야 한다.
+          /*이렇게 작성할 수도 있음!*/
+        />
+        //여기 쓰면 그대로 렌더링된다.
+        /*여기에선 주석 못 씀!*/
+      </div>
     );
   }
 }
