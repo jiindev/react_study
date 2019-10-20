438b1eb38b62d20faa02cd6752a65c5242987cb7
jiindev <jiindev@gmail.com>
2019-10-20 11:23:17 +0900

    state 설정 / 렌더링 / 업데이트(setState)

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -12,11 +12,30 @@ class MyComponent extends Component {
         list : PropTypes.array,
         everything : PropTypes.any
     }
+
+    state={
+        number:0
+    }
+
+    //constructor 메서드에서 정의하는 방법
+    // constructor(props){
+    //     super(props);
+    //     this.state={
+    //         number:0
+    //     }
+    // }
+
     render() {
         return (
             <div>
                 <p>안녕하세요? 나의 이름은 {this.props.name} 입니다.</p>
                 <p>저는 {this.props.age}살입니다.</p>
+                <p>숫자 : {this.state.number}</p>
+                <button onClick={()=>{
+                    this.setState({
+                        number:this.state.number + 1
+                    })
+                }}>더하기</button>
             </div>
         );
     }
