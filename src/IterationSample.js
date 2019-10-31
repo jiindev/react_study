f3eb1d0f9e150d5801606ea30ef65e171332a2d5
jiindev <jiindev@gmail.com>
2019-10-31 15:05:02 +0900

    state를 이용하여 데이터 추가 삭제 구현
    상태 안에서 배열을 변형할 땐 배열에 직접 접근하지 말고 concat, slice, filter등을 사용하여 새로운 배열을 만들고 setState 메서드로 적용한다.

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
 
 class IterationSample extends Component {
+    state={
+        names:['눈사람','얼음','눈','바람'],
+        name:''
+    };
+
+    handleChange = (e)=>{
+        this.setState({
+            name:e.target.value
+        });
+    }
+
+    handleInsert = ()=>{
+        this.setState({
+            names:this.state.names.concat(this.state.name),
+            name:''
+        });
+    }
+
+    handleRemove = (index)=>{
+        const {names} = this.state;
+        this.setState({
+            names:[
+                ...names.slice(0,index),
+                ...names.slice(index+1,names.length)
+            ]
+            
+        })
+    }
+
     render() {
-        const names=['눈사람','얼음','눈','바람']
-        const nameList = names.map(
-            (name,index)=>(<li key={index}>{name}</li>)
+        const nameList = this.state.names.map(
+            (name,index)=>(<li key={index} onDoubleClick={()=>this.handleRemove(index)}>{name}</li>)
         );
         return (
             <div>
-                {nameList}      
+                <input onChange = {this.handleChange}
+                value = {this.state.name}/>
+                <button onClick={this.handleInsert}>추가</button>
+                <ul>{nameList}</ul>
             </div>
         );
     }
