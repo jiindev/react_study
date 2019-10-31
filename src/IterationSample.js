70fc723bef2fa4f1b9f1d11435cca214ae97aaa3
jiindev <jiindev@gmail.com>
2019-10-31 15:06:20 +0900

    앞서 구현한 것을 filter를 이용하여 코드 더욱 간결하게 수정

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -22,11 +22,7 @@ class IterationSample extends Component {
     handleRemove = (index)=>{
         const {names} = this.state;
         this.setState({
-            names:[
-                ...names.slice(0,index),
-                ...names.slice(index+1,names.length)
-            ]
-            
+            names:names.filter((item,i)=>i!==index)
         })
     }
 
